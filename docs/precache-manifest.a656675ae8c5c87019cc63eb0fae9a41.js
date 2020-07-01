e9bd50d97cfa9a8984d346390e9acb6b64e0c11a
Brandon Rodriguez <brancowcanada@gmail.com>
2020-07-01 11:17:31 -0500

    docs

---
+++ docs/precache-manifest.a656675ae8c5c87019cc63eb0fae9a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbb88bd461ca05583bfdc44d117a773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f5a1a180019f34abbc",
+    "url": "/static/css/main.a1969cb0.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f5a1a180019f34abbc",
+    "url": "/static/js/main.c90d5269.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
